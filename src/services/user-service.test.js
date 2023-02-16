96c3dde5b328c3b8867a30bf158a6602699fe57a
Arief Romadhon <ariefromadhon26@gmail.com>
2023-02-16 20:51:44 +0700

    add unit test for getUserByUsername function

--- src/services/user-service.test.js
+++ src/services/user-service.test.js
@@ -2,6 +2,15 @@ import { hashSync } from 'bcrypt';
 import { User } from '../models';
 import UserService from './user-service';
 
+const mockUser = {
+  id: 'ae2cff11-9661-48e8-a51e-5b19b8b90633',
+  username: 'johndoe',
+  password: hashSync('password', 10),
+  email: 'johndoe@example.com',
+  token_version: 0,
+  refresh_token: 'refresh_token',
+};
+
 describe('getUsers function', () => {
   afterEach(() => {
     User.findAll.mockReset();
@@ -41,5 +50,29 @@ describe('getUsers function', () => {
 
     expect(result).toEqual(mockUsers);
     expect(User.findAll).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('getUserByUsername function', () => {
+  afterEach(() => {
+    User.findOne.mockReset();
+  });
+
+  it('should return user with matching username', async () => {
+    jest.spyOn(User, 'findOne').mockResolvedValue(mockUser);
+    const result = await UserService.getUserByUsername({ username: 'johndoe' });
+
+    expect(result).toHaveProperty('username', 'johndoe');
+    expect(User.findOne).toHaveBeenCalledTimes(1);
+    expect(User.findOne).toHaveBeenCalledWith({ where: { username: 'johndoe' } });
+  });
+
+  it('should return null if no user found', async () => {
+    jest.spyOn(User, 'findOne').mockResolvedValue(null);
+    const result = await UserService.getUserByUsername({ username: 'dummy' });
+
+    expect(result).toBeNull();
+    expect(User.findOne).toHaveBeenCalledTimes(1);
+    expect(User.findOne).toHaveBeenCalledWith({ where: { username: 'dummy' } });
   });
 });
