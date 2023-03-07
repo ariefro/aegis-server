cc120a3df368689ccf8ad9939cf04c0fb92f5360
ariefro <ariefromadhon26@gmail.com>
2023-03-07 11:26:25 +0700

    add validation before creating an account and change the return response

--- src/controllers/wallet-controller.js
+++ src/controllers/wallet-controller.js
@@ -69,9 +69,14 @@ class WalletController extends BaseController {
       const { name, balance, currency } = req.body;
       const { id: userID } = req.decoded;
 
+      const wallets = await WalletService.getWallets(userID);
+      if (wallets.length === 6) {
+        throw new Error(Errors.UnableToAddWallet);
+      }
+
       const cashFlow = await CashFlowService.addCashFlow();
       const cashFlowID = cashFlow.dataValues.id;
-      await WalletService.addWallet({
+      const wallet = await WalletService.addWallet({
         name,
         balance,
         currency,
@@ -79,7 +84,10 @@ class WalletController extends BaseController {
         cashFlowID,
       });
 
-      return res.send(this.reponseSuccess());
+      return res.send({
+        message: 'success',
+        wallet_id: wallet.id,
+      });
     } catch (err) {
       const error = this.getError(err);
 
@@ -98,7 +106,7 @@ class WalletController extends BaseController {
         throw new Error(Errors.WalletNotFound);
       }
 
-      const wallets = await WalletService.getOtherWallets(userID, id);
+      const wallets = await WalletService.getWallets(userID, id);
 
       const { rows: transactions, count: total_of_transaction } = await
       TransactionService.getTransactions(id, query);
