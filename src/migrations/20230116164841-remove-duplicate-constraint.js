d36fbb70f77530a10ef564d8e8d3e546eb8e8e6f
ariefro <ariefromadhon26@gmail.com>
2023-03-17 09:34:44 +0700

    change transaction model

--- src/migrations/20230116164841-remove-duplicate-constraint.js
+++ src/migrations/20230116164841-remove-duplicate-constraint.js
@@ -7,9 +7,25 @@ module.exports = {
   },
 
   down: async (queryInterface) => {
-    await queryInterface.addConstraint('transactions', 'transactions_to_wallet_id_fkey1');
-    await queryInterface.addConstraint('transactions', 'transactions_wallet_id_fkey1');
-    await queryInterface.addConstraint('wallets', 'wallets_cash_flow_id_fkey1');
-    await queryInterface.addConstraint('wallets', 'wallets_user_id_fkey1');
+    await queryInterface.addConstraint('transactions', {
+      fields: ['wallet_id'],
+      type: 'unique',
+      name: 'transactions_wallet_id_fkey1',
+    });
+    await queryInterface.addConstraint('transactions', {
+      fields: ['to_wallet_id'],
+      type: 'unique',
+      name: 'transactions_to_wallet_id_fkey1',
+    });
+    await queryInterface.addConstraint('wallets', {
+      fields: ['cash_flow_id'],
+      type: 'unique',
+      name: 'wallets_cash_flow_id_fkey1',
+    });
+    await queryInterface.addConstraint('wallets', {
+      fields: ['user_id'],
+      type: 'unique',
+      name: 'wallets_user_id_fkey1',
+    });
   },
 };
--- src/migrations/20230315105207-change-constraint-on-transaction-and-wallet-table.js
+++ src/migrations/20230315105207-change-constraint-on-transaction-and-wallet-table.js
@@ -1,11 +1,15 @@
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query('ALTER TABLE public.transactions DROP CONSTRAINT transactions_wallet_id_fkey;');
-    await queryInterface.sequelize.query('ALTER TABLE public.transactions ADD CONSTRAINT transactions_wallet_id_fkey FOREIGN KEY (wallet_id) REFERENCES public.wallets(id) ON DELETE SET NULL;');
+    await queryInterface.sequelize.query('ALTER TABLE public.transactions ADD CONSTRAINT transactions_wallet_id_fkey FOREIGN KEY (wallet_id) REFERENCES public.wallets(id) ON DELETE SET NULL ON UPDATE CASCADE;');
+    await queryInterface.sequelize.query('ALTER TABLE public.transactions DROP CONSTRAINT transactions_to_wallet_id_fkey;');
+    await queryInterface.sequelize.query('ALTER TABLE public.transactions ADD CONSTRAINT transactions_to_wallet_id_fkey FOREIGN KEY (to_wallet_id) REFERENCES public.wallets(id) ON DELETE SET NULL ON UPDATE CASCADE;');
   },
 
   down: async (queryInterface) => {
     await queryInterface.sequelize.query('ALTER TABLE public.transactions DROP CONSTRAINT transactions_wallet_id_fkey;');
     await queryInterface.sequelize.query('ALTER TABLE public.transactions ADD CONSTRAINT transactions_wallet_id_fkey FOREIGN KEY (wallet_id) REFERENCES public.wallets(id);');
+    await queryInterface.sequelize.query('ALTER TABLE public.transactions DROP CONSTRAINT transactions_to_wallet_id_fkey;');
+    await queryInterface.sequelize.query('ALTER TABLE public.transactions ADD CONSTRAINT transactions_to_wallet_id_fkey FOREIGN KEY (wallet_id) REFERENCES public.wallets(id);');
   },
 };
